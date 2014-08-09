eb3de00b5b8c0b99594b94bca774ab5609b049d2
oyuk <oyuk@users.noreply.github.com>
2014-08-09 14:55:40 +0900

    Create android_POST_parameta.java

---
+++ android_POST_parameta.java
@@ -0,0 +1,4 @@
+value.add(new BasicNameValuePair("user", "test"));
+		value.add(new BasicNameValuePair("start", spinner1.getSelectedItem().toString()));
+		value.add(new BasicNameValuePair("goal",  spinner2.getSelectedItem().toString()));
+		value.add(new BasicNameValuePair("temp", "temp"));
