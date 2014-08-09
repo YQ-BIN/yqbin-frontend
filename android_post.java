817009d12e9d2a498a315f165114e2f6d27c0fe2
k-masashi <masan580@gmail.com>
2014-08-09 14:52:34 +0900

    Update android_post.java

--- android_post.java
+++ android_post.java
@@ -1,21 +1,73 @@
-    private void post_word(){
-    	   HttpClient client = new DefaultHttpClient();
-         HttpPost post = new HttpPost("http://realfind.jp/p_insert.php");
-         // BODYに登録、設定
-         ArrayList<NameValuePair> value = new ArrayList<NameValuePair>();
-         value.add( new BasicNameValuePair("msg", hatugen));
-         value.add( new BasicNameValuePair("userkey", userkey));
+	public static String get_yahoo(String sentence,Integer type) {
+		String ret = "";
+		String comment = "";
+		// URL
+		URI url = null;
+		try {
+			url = new URI("http://yq-bin.sakura.ne.jp/order_insert.php");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			ret = e.toString();
+		}
 
-         String body = null;
-         try {
-              post.setEntity(new UrlEncodedFormEntity(value, "UTF-8"));
-              // リクエスト送信
-              HttpResponse response = client.execute(post);
-              // 取得
-              HttpEntity entity = response.getEntity();
-              body = EntityUtils.toString(entity, "UTF-8");
-         } catch(IOException e) {
-              e.printStackTrace();
-         }
-         client.getConnectionManager().shutdown();
-    }
+		// POSTパラメータ付きでPOSTリクエストを構築
+		HttpPost request = new HttpPost(url);
+		List<NameValuePair> post_params = new ArrayList<NameValuePair>();
+		value.add(new BasicNameValuePair("word", sentence));
+		value.add(new BasicNameValuePair("type", String.valueOf(type)));
+
+		try {
+			// 送信パラメータのエンコードを指定
+			request.setEntity(new UrlEncodedFormEntity(post_params, "UTF-8"));
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+
+		// POSTリクエストを実行
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		try {
+			// //Log.d("posttest", "POST開始");
+			ret = httpClient.execute(request, new ResponseHandler<String>() {
+
+				@Override
+				public String handleResponse(HttpResponse response)
+						throws IOException {
+
+					// 正常に受信できた場合は200
+					switch (response.getStatusLine().getStatusCode()) {
+					case HttpStatus.SC_OK:
+						// Log.d("posttest", "レスポンス取得に成功");
+						// レスポンスデータをエンコード済みの文字列として取得する
+						return EntityUtils.toString(response.getEntity(),
+								"UTF-8");
+
+					case HttpStatus.SC_NOT_FOUND:
+						// Log.d("posttest", "データが存在しない");
+						return null;
+
+					default:
+						// Log.d("posttest", "通信エラー");
+						// break;
+						return null;
+					}
+
+				}
+
+			});
+
+		} catch (IOException e) {
+			// Log.d("posttest", "通信に失敗：" + e.toString());
+		} finally {
+			// shutdownすると通信できなくなる
+			httpClient.getConnectionManager().shutdown();
+		}
+
+		if (ret == null) {
+			ret = "";
+			// SpeechText("取得できませんでした");
+		} else {
+
+		}
+		return ret;
+
+	}
